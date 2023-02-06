35c0df3d3df34fbffd1a25ae345d72149ea543fb
Markus Völk <markus.voelk@zwickroell.com>
2023-02-06 09:44:50 +0100

    Add steps to reproduce nx issue

--- packages/is-even/src/lib/is-even.spec.ts
+++ packages/is-even/src/lib/is-even.spec.ts
@@ -1,7 +1,11 @@
 import { isEven } from './is-even';
+import { when } from 'jest-when';
 
 describe('isEven', () => {
   it('should work', () => {
-    expect(isEven()).toEqual('is-even');
+    const spy = jest.fn();
+    when(spy).calledWith('foo').mockReturnValue('bar');
+    expect(spy('foo')).toEqual('bar');
+    expect(spy('unknown')).toBeUndefined();
   });
 });
